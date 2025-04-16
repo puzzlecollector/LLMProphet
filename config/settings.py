807c8892b2f531bd27148bdc0bcff8545a4d724b
puzzlecollector <puzzlecollector1123581321@gmail.com>
2025-04-17 00:17:31 +0900

    updated meta tag v5

--- config/settings.py
+++ config/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
